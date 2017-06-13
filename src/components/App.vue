7172b3fb2a98b756d5ec3bf56348f6c836d0aa6a
Narongsak <narongsak.chobsri@gmail.com>
2017-06-14 05:13:52 +0700

    Can select seat

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,7 +2,10 @@
   <div class="box">
     <h3 class="title">App: {{movieId}}</h3>
     <movie @chooseMovie="handleChooseMovie" :movieId="movieId"></movie>
-    <seat />
+    <seat :movieId="movieId" 
+          @chooseSeat="handleChooseSeat" 
+          :selectSeats="selectSeats"
+    />
   </div>
 </template>
 <script>
@@ -13,12 +16,22 @@ export default {
   components: { Movie, Seat },
   data() {
     return {
-      movieId: ''
+      movieId: '',
+      selectSeats: []
     }
   },
   methods: {
     handleChooseMovie(movieId) {
       this.movieId = movieId
+    },
+    handleChooseSeat(seat) {
+      const ids = this.selectSeats.map(s => s.id)
+      const idx = ids.indexOf(seat.id)
+      if(idx === -1) {
+        this.selectSeats.push(seat)
+      }else {
+        this.selectSeats.splice(idx,1)
+      }
     }
   }
 }
--- src/components/Seat.vue
+++ src/components/Seat.vue
@@ -1,5 +1,38 @@
 <template>
     <div class="box">
-        <h3 class="title">Seat</h3>
+        <h3 class="title">Seat: {{ movieId }}</h3>
+        <template v-for = "s in seats" >
+            <button
+                :class="className(s)"
+                :disabled="s.seated"
+                @click="chooseSeat(s)"
+            > {{ s.id }} {{s.price}} </button>
+            <span>&nbsp;</span>
+        </template>
     </div>
 </template>
+
+<script>
+    import movie from 'Others/movie.json'
+    export default {
+        props: [ 'movieId', 'selectSeats' ],
+        methods: {
+            className(seat) {
+                const ids = this.selectSeats.map(s => s.id)
+                const idx = ids.indexOf(seat.id)
+                return [
+                    "button",
+                    { "is-danger": seat.seated, "is-primary": idx != -1 }
+                ]
+            },
+            chooseSeat(seat) {
+                this.$emit('chooseSeat', seat)
+            }
+        },
+        computed: {
+            seats() {
+                return movie[this.movieId]
+            }
+        }
+    }
+</script>
