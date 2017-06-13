d63b53baf6951e55e9d81868e702ab945c046309
Narongsak <narongsak.chobsri@gmail.com>
2017-06-14 05:31:04 +0700

    Add summay price and count

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="box">
     <h3 class="title">App: {{movieId}}</h3>
+    <p> Count: {{status.count}} </p>
+    <p> Price: {{status.price}} </p>
     <movie @chooseMovie="handleChooseMovie" :movieId="movieId"></movie>
     <seat :movieId="movieId" 
           @chooseSeat="handleChooseSeat" 
@@ -17,21 +19,37 @@ export default {
   data() {
     return {
       movieId: '',
-      selectSeats: []
+      selectSeats: [],
+      status: { count: 0, price: 0 }
     }
   },
   methods: {
     handleChooseMovie(movieId) {
+      if(this.status.count) {
+        if( confirm("Data Will be lost ?") ) {
+            this.status = { count: 0, price: 0 }
+            this.selectSeats = []
+        }else {
+          return false
+        }
+      }
       this.movieId = movieId
     },
     handleChooseSeat(seat) {
       const ids = this.selectSeats.map(s => s.id)
       const idx = ids.indexOf(seat.id)
+
       if(idx === -1) {
         this.selectSeats.push(seat)
       }else {
         this.selectSeats.splice(idx,1)
       }
+
+      this.status = this.selectSeats.reduce((summary, s) => {
+        summary.count++
+        summary.price += s.price
+        return summary
+      }, { count: 0, price: 0 })
     }
   }
 }
