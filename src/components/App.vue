c968395c92bbd10929be975de20c01939cd0d073
Narongsak <narongsak.chobsri@gmail.com>
2017-06-14 03:58:13 +0700

    show photo each movie

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="box">
     <h3 class="title">App</h3>
+    <movie></movie>
   </div>
 </template>
+<script>
+import Movie from 'Components/Movie.vue'
+
+export default {
+  components: { Movie }
+}
+
+</script>
---
+++ src/components/Movie.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div v-for ="m in movies" class="column">
+                <figure class="image">
+                    <img :src="imgSrc(m.id)">
+                </figure>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { movies } from 'Others/movie.json'
+    export default {
+        data() {
+            return {
+                movies
+            }
+        },
+        methods: {
+            imgSrc(movieId) {
+                return `/movies/${ movieId }.jpg`
+            }
+        }
+    }
+</script>
